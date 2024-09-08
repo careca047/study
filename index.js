62990c8b02125042775462a5643979422d779b5e
careca <kaina.vieira@hotmail.com>
2024-09-08 16:51:34 -0300

    commit de teste

---
+++ index.js
@@ -0,0 +1,10 @@
+//let nomeestudante = "Helena";
+//console.log(nomeestudante);
+
+//const nomedoestudante = "Helena";
+//console.log(nomedoestudante);
+
+//string: nomeestudante = "Helena";
+
+console.log ("teste de console");
+
